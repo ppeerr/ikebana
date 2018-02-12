8f58d1fb74f1ac87768135655b0c3efee28cfe1b
PER <anton.kupreychik.af@gmail.com>
2018-02-13 02:19:39 +0700

    UI improve + enable bootstrap styles

--- src/main/resources/static/app/scripts/controllers/main.js
+++ src/main/resources/static/app/scripts/controllers/main.js
@@ -5,17 +5,17 @@ angular.module('ikebana')
         $scope.products = products;
         $scope.itemCounter = 0;
 
-        $scope.addToCart = function (product, stock) {
-            $scope.itemCounter += 1;
-            $scope.shoppingCart = {};
-            $scope.shoppingCart.productId = product.id;
-            $scope.shoppingCart.status = "NOT_PURCHASED";
-
-            if (angular.isUndefined(stock)) {
-                $scope.shoppingCart.stock = 1;
-            } else {
-                $scope.shoppingCart.stock = stock;
-            }
-            $http.post('http://localhost:8080/shoppingcart/shoppingCart', $scope.shoppingCart);
-        }
+        // $scope.addToCart = function (product, stock) {
+        //     $scope.itemCounter += 1;
+        //     $scope.shoppingCart = {};
+        //     $scope.shoppingCart.productId = product.id;
+        //     $scope.shoppingCart.status = "NOT_PURCHASED";
+        //
+        //     if (angular.isUndefined(stock)) {
+        //         $scope.shoppingCart.stock = 1;
+        //     } else {
+        //         $scope.shoppingCart.stock = stock;
+        //     }
+        //     $http.post('http://localhost:8080/shoppingcart/shoppingCart', $scope.shoppingCart);
+        // }
     }]);
